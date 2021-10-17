c32318a974e38809554021cb29b7d0f37ae5ef87
Vu Nguyen <vu.nguyenq89@gmail.com>
2021-10-17 21:50:18 +0200

    Day 09 part 1 done - standart Python list

--- 09.MarbleMania.py
+++ 09.MarbleMania.py
@@ -1,7 +1,7 @@
 import datetime
 
 exec_part = 1 # which part to execute
-exec_test_case = -1 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
+exec_test_case = 0 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
 
 # Puzzle input
 with open('input/input_test09.txt') as f:
@@ -11,16 +11,66 @@ with open('input/input09.txt') as f:
     INPUT = f.read()   
 
 def parse_input(input):
-    return input.split('\n')
+    return list(map(int, input.replace('players; last marble is worth ','').replace(' points','').split()))
+
+class MarbleCirle:
+    def __init__(self, no_of_players):
+        self.marble_list = [0]
+        self.current_marble = 0
+        self.current_player = 0
+        self.no_of_players = no_of_players
+        self.move_no = 0
+        self.scores = [0] * no_of_players
+        self.max_marble_point = 0
+    
+    # Return position of next n marbles from current_marble. +n => clockwise; -n => counter clockwise
+    def get_next_n_marble_pos(self, n):
+        pos = (self.current_marble + n) % len(self.marble_list)
+        if pos < 0: # Convert negative list index to positive
+            pos += len(self.marble_list)
+        return (self.current_marble + n) % len(self.marble_list)
+
+    def add_regular_marble(self, marble_value):
+        self.current_marble = self.get_next_n_marble_pos(2)
+        self.marble_list = self.marble_list[:self.current_marble] + [marble_value] + self.marble_list[self.current_marble:]
+
+    def take_turn(self):
+        self.move_no += 1
+        if (self.move_no % 23) == 0:
+            self.current_marble = self.get_next_n_marble_pos(-7)
+            removed_marble = self.marble_list.pop(self.current_marble)
+            self.max_marble_point = self.move_no + removed_marble
+            self.scores[self.current_player] += self.max_marble_point
+        else:
+            self.add_regular_marble(self.move_no)
+        self.current_player = (self.current_player + 1) % self.no_of_players
+    
+    def print_circle(self):
+        start_marble = min(self.marble_list)
+        start_pos = self.marble_list.index(start_marble)
+        print(f"Player: {self.current_player}. Last marble point: {self.max_marble_point}. Circle: {self.marble_list[start_pos:] + self.marble_list[0:start_pos]}")
+            
+
 
 def part1(input):
+    no_players, last_marble_worth = input
     print(input)
-    result = 0
-    return result
+    circle = MarbleCirle(no_players)
+    for i in range(last_marble_worth + 1):
+        circle.take_turn()
+        #circle.print_circle()
+        # Progress tracking
+        if (i%1000) == 0:
+            print(f"{i}/{last_marble_worth}")
+    return max(circle.scores)
 
 def part2(input):
-    result = 0
-    return result
+    no_players, last_marble_worth = input
+    print(input)
+    circle = MarbleCirle(no_players)
+    for i in range(last_marble_worth + 1):
+        circle.take_turn()
+    return max(circle.scores)
 
 if __name__ == "__main__":
     if(exec_test_case == 0):
