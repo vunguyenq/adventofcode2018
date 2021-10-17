ed0753b482bd0e6509e4c13654892df6d26fa739
Vu Nguyen <vu.nguyenq89@gmail.com>
2021-10-17 10:58:48 +0200

    Updated README

---
+++ 09.MarbleMania.py
@@ -0,0 +1,38 @@
+import datetime
+
+exec_part = 1 # which part to execute
+exec_test_case = 1 # 1 = test input; 0 = real puzzle input
+
+# Puzzle input
+with open('input/input_test09.txt') as f:
+    INPUT_TEST = f.read()
+
+with open('input/input09.txt') as f:
+    INPUT = f.read()   
+
+def parse_input(input):
+    return input.split('\n')
+
+def part1(input):
+    result = 0
+    return result
+
+def part2(input):
+    result = 0
+    return result
+
+if __name__ == "__main__":
+    if(exec_test_case == 1):
+        input = INPUT_TEST
+    else:
+        input = INPUT
+    input = parse_input(input)
+
+    start_time = datetime.datetime.now() 
+    if (exec_part == 1):
+        result = part1(input)
+    else:
+        result = part2(input)
+    end_time = datetime.datetime.now() 
+    print('Part {} time: {}'.format(exec_part, end_time - start_time))
+    print('Part {} answer: {}'.format(exec_part, result))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adventofcode2018
+Run make on a BASH shell
+Create new day code: make create dayname="01 day1_title"
+Delete day code: make delete day=01
