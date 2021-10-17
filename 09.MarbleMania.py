a8c892a1127420b4ae10c778eacf85488aa9ed9f
Vu Nguyen <vu.nguyenq89@gmail.com>
2021-10-17 11:35:26 +0200

    Improved code template to enable multiple testcases

--- 09.MarbleMania.py
+++ 09.MarbleMania.py
@@ -1,7 +1,7 @@
 import datetime
 
 exec_part = 1 # which part to execute
-exec_test_case = 1 # 1 = test input; 0 = real puzzle input
+exec_test_case = -1 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
 
 # Puzzle input
 with open('input/input_test09.txt') as f:
@@ -14,6 +14,7 @@ def parse_input(input):
     return input.split('\n')
 
 def part1(input):
+    print(input)
     result = 0
     return result
 
@@ -22,17 +23,32 @@ def part2(input):
     return result
 
 if __name__ == "__main__":
-    if(exec_test_case == 1):
-        input = INPUT_TEST
+    if(exec_test_case == 0):
+        inputs = [INPUT]
     else:
-        input = INPUT
-    input = parse_input(input)
+        inputs = INPUT_TEST.split("\n#####INPUT_SEPERATOR#####\n")
+    
+    if exec_test_case > len(inputs):
+        print(f"Test case {exec_test_case} does not exist")
+        quit()
+    for i, input_str in enumerate(inputs):
+        if(exec_test_case == 0):
+            print(f"Running real puzzle input...")
+        elif (exec_test_case == -1):
+            print(f"Running test case {i+1}/{len(inputs)}...")
+        else:
+            if (i+1 == exec_test_case):
+                print(f"Running test case {i+1}/{len(inputs)}...")
+            else:
+                continue
+            
+        input = parse_input(input_str)
+        start_time = datetime.datetime.now() 
+        if (exec_part == 1):
+            result = part1(input)
+        else:
+            result = part2(input)
+        end_time = datetime.datetime.now() 
 
-    start_time = datetime.datetime.now() 
-    if (exec_part == 1):
-        result = part1(input)
-    else:
-        result = part2(input)
-    end_time = datetime.datetime.now() 
-    print('Part {} time: {}'.format(exec_part, end_time - start_time))
-    print('Part {} answer: {}'.format(exec_part, result))
+        print('Part {} time: {}'.format(exec_part, end_time - start_time))
+        print('Part {} answer: {}\n'.format(exec_part, result))
--- README.md
+++ README.md
@@ -7,4 +7,17 @@ Create new day code:
 <code>make create dayname="01 DayTitle"</code>  
   
 Delete day code:  
-<code>make delete day=01.</code>    
+<code>make delete day=01</code>    
+  
+If there is more than 1 test case, sepearte test cases in file <code>input_testxx.txt</code> with <code>#####INPUT_SEPERATOR#####</code>.  
+Example - <code>input_test01.txt</code> with 3 test cases:  
+<code>
+1 2
+3 4
+#####INPUT_SEPERATOR#####
+5 6
+7 8
+#####INPUT_SEPERATOR#####
+9 10
+11 12
+</code>
